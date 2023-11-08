9470f390258cb182efbdd0893483aa0af0c28096
Gabriella Maris <gabriella.maris@etec.sp.gov.br>
2023-11-08 16:29:11 -0300

    vetor

---
+++ dezvetores.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int vetores[10];
+	int i;
+//Ler o numero do usuario e calcular o dobro
+
+for (i = 0; i < 10; i++); {
+	printf("Digite o numero na posicao %d:"; i);
+	scanf("%d", &vetor[i]);
+	vetor[i] *= 2;
+	}
+//Apresentar os resultados
+	printf("Os numeros dobrados sao \n");
+	for(i = 0; i < 10; i++); {
+		printf("%d\n", vetor[i]);
+	}
+	return 0;
+} 
---
+++ programa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int posicao[10];
+float numero;
+
+printf("digite um número para a posição 1");
+scanf("%f",&valor1);
+
+for (i = 0; i < 10; i = i+1){
+printf("Valor de i é %d",
+
+
+
